8ca0198e8d3b7b6f8309982c8fb33d364b84d179
Sean Uz <cuz3@gatech.edu>
2024-01-02 00:53:37 -0500

    Make buttons responsive

---
+++ calcScript.js
@@ -0,0 +1,28 @@
+const displayField = document.querySelector(".display");
+
+const baseList = document.querySelectorAll(".number, .classic, #root, #closepar, #openpar, #pi, #percent, #dot");
+baseList.forEach(button => {
+    button.addEventListener("click", event => {
+        displayField.value += event.target.textContent;
+    });
+});
+
+const mod = document.querySelector("#mod");
+mod.addEventListener("click", event => {
+    displayField.value += ` ${event.target.textContent} `;
+});
+
+const erase = document.querySelector("#erase");
+erase.addEventListener("click", event => {
+    displayField.value = "";
+});
+
+const square = document.querySelector("#square");
+square.addEventListener("click", event => {
+    displayField.value += "^";
+});
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", event => {
+    displayField.value = "=";
+});
