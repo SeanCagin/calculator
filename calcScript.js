a11d7d0b3c2087b340e3136a678165593690c423
Sean Uz <cuz3@gatech.edu>
2024-01-03 00:51:49 -0500

    Add keyboard functionality by changing button id's to ascii codes

--- calcScript.js
+++ calcScript.js
@@ -1,6 +1,23 @@
-const displayField = document.querySelector(".display");
+function enterKey(event) {
+    const char = event.key;
+    let buttonId = "#";
+    if (validKeyInput.has(char)) {
+        if (char === "Delete" || char === "Backspace") buttonId += "delete";
+        else if (char === "Enter") buttonId += "equals";
+        else buttonId += `c${char.charCodeAt(0)}`;
+    } else {
+        return;
+    }
+    let clickEvent = new Event("click");
+    const eventButton = document.querySelector(buttonId);
+    eventButton.dispatchEvent(clickEvent);
+}
 
-const baseList = document.querySelectorAll(".number, .classic, #root, #closepar, #openpar, #pi, #percent, #dot");
+
+const displayField = document.querySelector(".display");
+const validKeyInput = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/", "+", "^", "-", "*", ".", "%", "(", ")", "Delete", "Backspace", "Enter"]);
+
+const baseList = document.querySelectorAll(".number, .classic, #root, #c40, #c41, #pi, #c37, #c46");
 baseList.forEach(button => {
     button.addEventListener("click", event => {
         displayField.value += event.target.textContent;
@@ -12,12 +29,12 @@ mod.addEventListener("click", event => {
     displayField.value += ` ${event.target.textContent} `;
 });
 
-const erase = document.querySelector("#erase");
+const erase = document.querySelector("#delete");
 erase.addEventListener("click", event => {
     displayField.value = "";
 });
 
-const square = document.querySelector("#square");
+const square = document.querySelector("#c94");
 square.addEventListener("click", event => {
     displayField.value += "^";
 });
@@ -27,3 +44,4 @@ equals.addEventListener("click", event => {
     displayField.value = "=";
 });
 
+document.addEventListener("keydown", enterKey);
